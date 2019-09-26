a550ad1dda671a6e7febd1bd0020c3df8f70e810
Anshor <anshorjihadi@gmail.com>
2019-09-26 18:28:33 +0700

    Tugas 8 Js

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function tampil() {
+  var motor = {
+    Merk: "Yamaha",
+    Type: "Mio",
+    Tahun: 2009,
+    }
+
+    motor.keterangan = "Kondisi Mulus"
+    console.log(motor);
+}
+
+tampil()
